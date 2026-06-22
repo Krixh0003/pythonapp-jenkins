18ffaa98941d6f18376d13aabe562dd4ae45b0d6
krish1 <vasisthkrish92@gmail.com>
2026-06-22 11:35:45 +0530

    fixed app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-  GNU nano 9.0                         app.py
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Jenkins CI/CD Pipeline!"
+    return "Hello from Jenkins!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
